2930567e087b732ae4194ab453ee6e7d7a400c75
pricelees <priceelees@gmail.com>
2024-08-27 15:24:08 +0900

    feat: origin 확인을 위한 API 추가

--- backend/src/main/java/mouda/backend/config/HeaderCheckController.java
+++ backend/src/main/java/mouda/backend/config/HeaderCheckController.java
@@ -11,10 +11,21 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class HeaderCheckController {
 
-	@GetMapping("/headers")
+	@GetMapping("/v1/headers")
 	public String headers(HttpServletRequest request) {
 		return Collections.list(request.getHeaderNames()).stream()
 			.map(name -> name + ": " + Collections.list(request.getHeaders(name)))
 			.collect(Collectors.joining("<br>"));
+	}
+
+	@GetMapping("/v1/headers/froxy")
+	public String froxyHeaders(HttpServletRequest request) {
+		String scheme = request.getScheme();
+		String host = request.getServerName();
+		int port = request.getServerPort();
+
+		return String.format("scheme: %s<br>", scheme)
+			+ String.format("host: %s<br>", host)
+			+ String.format("port: %d<br>", port);
 	}
 }
