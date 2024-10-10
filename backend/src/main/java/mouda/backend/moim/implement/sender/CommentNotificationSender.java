0b802398b729176ee7b94d5e60273a8de123f7fa
pricelees <priceelees@gmail.com>
2024-10-10 18:25:58 +0900

    refactor: 댓글 알림에서의 분기처리 수정

--- backend/src/main/java/mouda/backend/moim/implement/sender/CommentNotificationSender.java
+++ backend/src/main/java/mouda/backend/moim/implement/sender/CommentNotificationSender.java
@@ -8,34 +8,41 @@ import org.springframework.stereotype.Component;
 import mouda.backend.common.config.UrlConfig;
 import mouda.backend.darakbangmember.domain.DarakbangMember;
 import mouda.backend.moim.domain.Comment;
+import mouda.backend.moim.domain.CommentRecipient;
+import mouda.backend.moim.domain.Moim;
 import mouda.backend.moim.implement.finder.CommentRecipientFinder;
 import mouda.backend.notification.domain.NotificationEvent;
 import mouda.backend.notification.domain.NotificationType;
 import mouda.backend.notification.domain.Recipient;
 
 @Component
-public class CommentNotificationSender extends AbstractNotificationSender{
+public class CommentNotificationSender extends AbstractNotificationSender {
 
 	private final CommentRecipientFinder commentRecipientFinder;
 	private final ApplicationEventPublisher eventPublisher;
 
-	public CommentNotificationSender(UrlConfig urlConfig, CommentRecipientFinder commentRecipientFinder, ApplicationEventPublisher eventPublisher) {
+	public CommentNotificationSender(UrlConfig urlConfig, CommentRecipientFinder commentRecipientFinder,
+		ApplicationEventPublisher eventPublisher) {
 		super(urlConfig);
 		this.commentRecipientFinder = commentRecipientFinder;
 		this.eventPublisher = eventPublisher;
 	}
 
 	public void sendCommentNotification(Comment comment, DarakbangMember author) {
-		List<Recipient> recipients;
-		NotificationType notificationType;
-		if (comment.isChild()) {
-			recipients = commentRecipientFinder.getNewCommentNotificationRecipients(comment.getMoim().getId(), author);
-			notificationType = NotificationType.NEW_COMMENT;
-		} else {
-			recipients = commentRecipientFinder.getNewReplyNotificationRecipients(comment);
-			notificationType = NotificationType.NEW_REPLY;
-		}
-		NotificationEvent notificationEvent = new NotificationEvent(notificationType, comment.getMoim().getTitle(), notificationType.createMessage(author.getNickname()), getMoimUrl(author.getDarakbang().getId(), comment.getMoim().getId()), recipients);
+		List<CommentRecipient> commentRecipients = commentRecipientFinder.getAllRecipient(comment);
+
+		commentRecipients.forEach(commentRecipient -> {
+			sendNotification(commentRecipient, comment, author);
+		});
+	}
+
+	private void sendNotification(CommentRecipient commentRecipient, Comment comment, DarakbangMember author) {
+		NotificationType notificationType = commentRecipient.getNotificationType();
+		String message = notificationType.createMessage(author.getNickname());
+		List<Recipient> recipients = commentRecipient.getRecipients();
+		Moim moim = comment.getMoim();
+		NotificationEvent notificationEvent = new NotificationEvent(notificationType, moim.getTitle(), message,
+			getMoimUrl(moim.getDarakbangId(), moim.getId()), recipients);
 
 		eventPublisher.publishEvent(notificationEvent);
 	}
